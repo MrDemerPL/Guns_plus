ef3adf6dd89b327fc5bc7abd0e82d057113dda0c
atlan1 <atl4n1@gmail.com>
2013-03-22 09:01:33 +0100

    - Changed the way additions modify gun properties. - Ammo has now a durability bar -> magazines - Guns can have durability aswell, which means their lifespan is limited - Greatly improved target detection aswell as headshot detection by using bounding boxes of the targets - Formatting - HUD now shows current mag size / total amount - current mag size - Added some more utilitie methods
    Signed-off-by: atlan1 <atl4n1@gmail.com>

--- Guns+/src/main/java/team/GunsPlus/Enum/FireBehavior.java
+++
@@ -1,6 +0,0 @@
-package team.GunsPlus.Enum;
-
-public enum FireBehavior {
-
-	SINGLE(), AUTOMATIC();
-}
--- Guns+/src/main/java/team/GunsPlus/Item/Gun.java
+++
@@ -1,7 +0,0 @@
-package team.GunsPlus.Item;
-
-import team.ApiPlus.API.EffectHolder;
-import team.ApiPlus.API.PropertyHolder;
-
-public interface Gun extends PropertyHolder, EffectHolder {
-}
